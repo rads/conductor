fd8e4d6a4c3cc015bbeb840d88002f9bcb6f15d0
Radford Smith <contact@radsmith.com>
2017-10-27 20:39:46 -0700

    Remove auto-instrument in CLJS (use instrument instead)

--- src/conductor_cljs/alpha.clj
+++ src/conductor_cljs/alpha.clj
@@ -5,19 +5,10 @@
   (or (System/getProperty "conductor.cljs-test-ns")
       "orchestra-cljs.spec.test"))
 
-(def printer-fn-impl
+(def printer-fn
   (let [printer (some-> (System/getProperty "conductor.printer-fn")
                         (edn/read-string))]
     (or printer 'expound.alpha/printer)))
-
-(defmacro printer-fn []
-  printer-fn-impl)
-
-(defmacro instrument []
-  `(~(symbol test-ns "instrument")))
-
-(defmacro unstrument []
-  `(~(symbol test-ns "unstrument")))
 
 (defmacro fixture [sym-or-syms]
   `(let [original-explain# (atom nil)
@@ -27,7 +18,7 @@
       #(let [formatter# conductor-cljs.alpha/test-formatter]
          (reset! original-explain# cljs.spec.alpha/*explain-out*)
          (cljs.spec.alpha/check-asserts true)
-         (set! cljs.spec.alpha/*explain-out* ~printer-fn-impl)
+         (set! cljs.spec.alpha/*explain-out* ~printer-fn)
          (cljs.test/update-current-env! [:formatter] (constantly formatter#))
          (~(symbol test-ns "instrument") ~sym-or-syms))
 
@@ -37,3 +28,24 @@
          (cljs.spec.alpha/check-asserts original-assert#)
          (set! cljs.spec.alpha/*explain-out* @original-explain#)
          (cljs.test/update-current-env! [:formatter] (constantly original-formatter#)))}))
+
+(defmacro instrument []
+  `(let [instrumented# (~(symbol test-ns "instrument"))]
+     (when-not @enabled
+       (reset! original-formatter (:formatter (cljs.test/get-current-env)))
+       (reset! original-assert (cljs.spec.alpha/check-asserts?))
+       (reset! original-explain cljs.spec.alpha/*explain-out*)
+       (cljs.spec.alpha/check-asserts true)
+       (set! cljs.spec.alpha/*explain-out* ~printer-fn)
+       (cljs.test/update-current-env! [:formatter] (constantly test-formatter))
+       (reset! enabled true))
+     instrumented#))
+
+(defmacro unstrument []
+  `(when @enable
+     (cljs.spec.alpha/check-asserts @original-assert)
+     (set! cljs.spec.alpha/*explain-out* @original-explain)
+     (cljs.test/update-current-env! [:formatter] (constantly @original-formatter))
+     (~(symbol test-ns "instrument"))
+     (reset! enabled false)
+     nil))
--- src/conductor_cljs/alpha.cljs
+++ src/conductor_cljs/alpha.cljs
@@ -3,7 +3,7 @@
    [cljs.spec.alpha :as s]
    [cljs.spec.test.alpha :as stest]
    [cljs.test :as test])
-  (:require-macros [conductor-cljs.alpha :refer [instrument unstrument printer-fn]]))
+  (:require-macros [conductor-cljs.alpha]))
 
 (def ^:private enabled (atom false))
 
@@ -16,28 +16,3 @@
     (.-message v)
     v))
 
-(defn- start-instrument []
-  (let [instrumented (instrument)]
-    (when-not @enabled
-      (reset! original-formatter (:formatter (test/get-current-env)))
-      (reset! original-assert (s/check-asserts?))
-      (reset! original-explain s/*explain-out*)
-      (s/check-asserts true)
-      (set! s/*explain-out* (printer-fn))
-      (test/update-current-env! [:formatter] (constantly test-formatter))
-      (reset! enabled true))
-    instrumented))
-
-(defn- stop-instrument []
-  (when @enabled
-    (s/check-asserts @original-assert)
-    (set! s/*explain-out* @original-explain)
-    (test/update-current-env! [:formatter] (constantly @original-formatter))
-    (unstrument)
-    (reset! enabled false)
-    nil))
-
-(defn auto-instrument [flag]
-  (if flag
-    (start-instrument)
-    (stop-instrument)))
