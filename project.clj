b9b939c3d636c82ea49d208767258d38f343aadd
Radford Smith <contact@radsmith.com>
2017-11-02 14:22:33 -0700

    Relase 0.1.2

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject conductor "0.1.1-SNAPSHOT"
+(defproject conductor "0.1.2"
   :description "Automatic instrumentation for clojure.spec"
   :url "https://github.com/rads/conductor"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[expound "0.3.1"]
                  [orchestra "2017.08.13"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-beta4"]
                                   [org.clojure/clojurescript "1.9.946"]]}})
